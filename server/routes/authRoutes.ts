db8c89721238cc046b5e0906f6ef0a0b6989e58b
Claire Karsenti <claire.karsenti.web@gmail.com>
2023-09-30 21:56:14 +0200

    Typo

--- server/routes/authRoutes.ts
+++ server/routes/authRoutes.ts
@@ -3,7 +3,7 @@ import { register, login } from '../controllers/authController';
 
 const router = express.Router();
 
-router.post('/signup', register);
+router.post('/register', register);
 router.post('/login', login);
 
 export default router;
