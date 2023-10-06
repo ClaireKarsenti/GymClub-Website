655c37ee1297801096cfac1cb6642a1681c96d55
Claire Karsenti <claire.karsenti.web@gmail.com>
2023-10-06 18:05:46 +0200

    Add auth middleware

--- server/dist/routes/authRoutes.js
+++ server/dist/routes/authRoutes.js
@@ -5,7 +5,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const authController_1 = require("../controllers/authController");
+const auth_js_1 = require("../middleware/auth.js");
 const router = express_1.default.Router();
 router.post('/register', authController_1.register);
-router.post('/login', authController_1.login);
+router.post('/login', auth_js_1.verifyToken, authController_1.login);
 exports.default = router;
--- server/routes/authRoutes.ts
+++ server/routes/authRoutes.ts
@@ -1,9 +1,10 @@
 import express from 'express';
 import { register, login } from '../controllers/authController';
+import { verifyToken } from '../middleware/auth.js';
 
 const router = express.Router();
 
 router.post('/register', register);
-router.post('/login', login);
+router.post('/login', verifyToken, login);
 
 export default router;
